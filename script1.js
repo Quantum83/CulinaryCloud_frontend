ec3204f2092eac36afafb29b08c6a9cecb14c669
Quantum83 <carbidenarwhal@gmail.com>
2024-06-10 07:12:16 -0700

    a test for sign in redirect

--- script1.js
+++ script1.js
@@ -32,9 +32,11 @@ function redirectToCognitoSignIn() {
   const redirectUri = "https://culinary-cloud.click/profile.html";
   const responseType = "code";
 
-  const cognitoUrl = `https://${domain}/login?client_id=${clientId}&response_type=${responseType}&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
-    redirectUri
-  )}`;
+  // const cognitoUrl = `https://${domain}/login?client_id=${clientId}&response_type=${responseType}&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
+  //   redirectUri
+  // )}`;
+  const cognitoUrl =
+    "https://culinary-cloud.auth.us-east-1.amazoncognito.com/login?client_id=3fj031gafrt8bae5k4cflh1281&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fculinary-cloud.click%2Fprofile.html";
   window.location.href = cognitoUrl;
 }
 
