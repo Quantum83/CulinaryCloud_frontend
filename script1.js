0c9090293aa54f8e320f0cd6677beabb11d5f957
Quantum83 <carbidenarwhal@gmail.com>
2024-06-10 08:11:06 -0700

    ive lost hope

--- script1.js
+++ script1.js
@@ -32,11 +32,11 @@ function redirectToCognitoSignIn() {
   const redirectUri = "https://culinary-cloud.click/profile.html";
   const responseType = "code";
 
-  const cognitoUrl = `https://${domain}/login?client_id=${clientId}&response_type=${responseType}&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
+  const WhereToGoUrl = `https://${domain}/login?client_id=${clientId}&response_type=${responseType}&scope=email+openid+phone&redirect_uri=${encodeURIComponent(
     redirectUri
   )}`;
-  console.log(cognitoUrl);
-  window.location.href = cognitoUrl;
+  console.log(WhereToGoUrl);
+  window.location.href = WhereToGoUrl;
 }
 
 function sortRecipes() {
